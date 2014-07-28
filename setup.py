bb6593ce18489bb752a48269730348b913e18c8c
royendgelsilberie <royendgel@gmail.com>
2014-07-27 23:27:49 -0400

    ping, tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "mjpeg-tools",
-    version = "0.0.10",
+    version = "0.0.11",
     author = "Royendgel Silberie",
     author_email = "royendgel@techprocur.com",
     description = ("mjpeg client, server and manipulator"),
